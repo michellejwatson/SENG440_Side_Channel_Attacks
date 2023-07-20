52575219e02d227e4f5d55f4e5560cfdbae783cf
Michelle Watson <michellejwatson1@gmail.com>
2023-07-19 22:09:52 -0700

    change back to int64_t

--- main.c
+++ main.c
@@ -3,23 +3,10 @@
 ** No implementation for hiding or masking of execution time / power consumption 
 */
 
-#include "arm_neon.h"
+//#include "arm_neon.h"
 #include <stdio.h>
 #include <stdint.h>
 #include <time.h>
-//#include "RSA_functions.h"
-
-/** Performs Montgomery modular multiplication **/
-long long int montgomery_multiplication(long long int a, long long int b, long long int modulus);
-
-/** Computes the modular inverse of a number **/
-long long int compute_modular_inverse(long long int number, long long int modulus);
-
-/** Performs Montgomery modular reduction **/
-long long int montgomery_modular_reduction(long long int result, long long int modulus, long long int Y, long long int m);
-
-/** Performs Montgomery modular exponentiatio **/
-long long int montgomery_modular_exponentiation(long long int base, long long int exponent, long long int modulus, long long int Y, long long int m);
 
 /** 
  * Performs Montgomery modular multiplication.
@@ -29,9 +16,9 @@ long long int montgomery_modular_exponentiation(long long int base, long long in
  *   - modulus: The modulus.
  * Returns: The result of the multiplication.
  */
-long long int montgomery_multiplication(long long int a, long long int b, long long int modulus) {
-    long long int result = 0;
-    long long int factor = (1ULL << 32) % modulus;
+int64_t montgomery_multiplication(int64_t a, int64_t b, int64_t modulus) {
+    int64_t result = 0;
+    int64_t factor = (1ULL << 32) % modulus;
     int i;
 
     for (i = 0; i < 32; i++) {
@@ -55,18 +42,18 @@ long long int montgomery_multiplication(long long int a, long long int b, long l
  *   - modulus: The modulus.
  * Returns: The modular inverse of the number, or 0 if it does not exist.
  */
-long long int compute_modular_inverse(long long int number, long long int modulus) {
+int64_t compute_modular_inverse(int64_t number, int64_t modulus) {
     int64_t t = 0, new_t = 1;
-    long long int r = modulus, new_r = number;
+    int64_t r = modulus, new_r = number;
 
     while (new_r != 0) {
-        long long int quotient = r / new_r;
+        int64_t quotient = r / new_r;
 
-        long long int temp_t = new_t;
+        int64_t temp_t = new_t;
         new_t = t - quotient * new_t;
         t = temp_t;
 
-        long long int temp_r = new_r;
+        int64_t temp_r = new_r;
         new_r = r - quotient * new_r;
         r = temp_r;
     }
@@ -92,13 +79,13 @@ long long int compute_modular_inverse(long long int number, long long int modulu
  *   - m: The number of bits.
  * Returns: The reduced result after Montgomery modular reduction.
  */
-long long int montgomery_modular_reduction(long long int result, long long int modulus, long long int Y, long long int m) {
-    long long int factor = (1ULL << m) % modulus;
-    long long int R_inverse = compute_modular_inverse(factor, modulus); // Compute the modular inverse of the Montgomery factor R
+int64_t montgomery_modular_reduction(int64_t result, int64_t modulus, int64_t Y, int64_t m) {
+    int64_t factor = (1ULL << m) % modulus;
+    int64_t R_inverse = compute_modular_inverse(factor, modulus); // Compute the modular inverse of the Montgomery factor R
 
-    long long int montgomery_result = montgomery_multiplication(result, 1, modulus); // Convert the result to Montgomery form
+    int64_t montgomery_result = montgomery_multiplication(result, 1, modulus); // Convert the result to Montgomery form
 
-    long long int reduced_result = montgomery_multiplication(montgomery_result, R_inverse, modulus); // Perform the Montgomery reduction
+    int64_t reduced_result = montgomery_multiplication(montgomery_result, R_inverse, modulus); // Perform the Montgomery reduction
 
     return reduced_result;
 }
@@ -113,10 +100,10 @@ long long int montgomery_modular_reduction(long long int result, long long int m
  *   - m: The number of bits.
  * Returns: The result of the modular exponentiation.
  */
-long long int montgomery_modular_exponentiation(long long int base, long long int exponent, long long int modulus, long long int Y, long long int m) {
-    long long int result = 1;
-    long long int R = (1ULL << m) % modulus;
-    long long int baseMont = montgomery_multiplication(base, R, modulus);  // R is the Montgomery factor
+int64_t montgomery_modular_exponentiation(int64_t base, int64_t exponent, int64_t modulus, int64_t Y, int64_t m) {
+    int64_t result = 1;
+    int64_t R = (1ULL << m) % modulus;
+    int64_t baseMont = montgomery_multiplication(base, R, modulus);  // R is the Montgomery factor
 
     while (exponent > 0) {
         if (exponent & 1) {
@@ -132,14 +119,14 @@ long long int montgomery_modular_exponentiation(long long int base, long long in
 
 int main() {
     /** initial test **/
-    long long int P = 61; // Prime Number P
-    long long int Q = 53; // Prime Number Q
-    long long int N = P * Q; // Modulus N
-    long long int phi = (P - 1) * (Q - 1); // Euler's totient function value
-    long long int E = 17; // Public exponent
-    long long int plaintext = 440;
-    long long int m = 64; // 64 bits
-    long long int D;
+    int64_t P = 61; // Prime Number P
+    int64_t Q = 53; // Prime Number Q
+    int64_t N = P * Q; // Modulus N
+    int64_t phi = (P - 1) * (Q - 1); // Euler's totient function value
+    int64_t E = 17; // Public exponent
+    int64_t plaintext = 440;
+    int64_t m = 64; // 64 bits
+    int64_t D;
 
     // Check 1 < E < PQ
     if (E <= 1 || E >= N) {
@@ -154,7 +141,7 @@ int main() {
     }
 
     // Calculate the private exponent d using the formula D = (X(P-1)(Q-1) + 1) / E
-    long long int X = 1;
+    int64_t X = 1;
     while ((X * (P - 1) * (Q - 1) + 1) % E != 0) {
         X++;
     }
@@ -165,19 +152,19 @@ int main() {
     // Private Key: (D, PQ)
 
     // Compute R = (2^m) % N
-    long long int R = 1;
+    int64_t R = 1;
     int i;
     for (i = 0; i < m; i++) {
         R = (R << 1) % N;
     }
 
     // Compute Y = (R^2) % N
-    long long int Y = (R * R) % N;
+    int64_t Y = (R * R) % N;
 
     // Perform RSA encryption
     clock_t start_encrypt = clock();
 
-    long long int ciphertext = montgomery_modular_exponentiation(plaintext, E, N, Y, m);
+    int64_t ciphertext = montgomery_modular_exponentiation(plaintext, E, N, Y, m);
 
     clock_t end_encrypt = clock();
     double total_time_encrypt = (double)(end_encrypt - start_encrypt) / CLOCKS_PER_SEC;
@@ -189,7 +176,7 @@ int main() {
     // Perform RSA decryption
     clock_t start_decrypt = clock();
 
-    long long int decrypted = montgomery_modular_exponentiation(ciphertext, D, N, Y, m);
+    int64_t decrypted = montgomery_modular_exponentiation(ciphertext, D, N, Y, m);
 
     clock_t end_decrypt = clock();
     double total_time_decrypt = (double)(end_decrypt - start_decrypt) / CLOCKS_PER_SEC;
