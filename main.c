f436a2e630547f2b282fefb6e60ef9c7cf18802e
Michelle Watson <michellejwatson1@gmail.com>
2023-07-20 10:34:05 -0700

    switch to uint32_t

--- main.c
+++ main.c
@@ -16,12 +16,10 @@
  *   - modulus: The modulus.
  * Returns: The result of the multiplication.
  */
-int64_t montgomery_multiplication(int64_t a, int64_t b, int64_t modulus) {
-    int64_t result = 0;
-    int64_t factor = (1LL << 32) % modulus;
+uint32_t montgomery_multiplication(uint32_t a, uint32_t b, uint32_t modulus) {
+    uint32_t result = 0;
+    uint32_t factor = (1LL << 32) % modulus;
     int i;
-
-    printf("in montgomery multiplication\n");
 
     for (i = 0; i < 32; i++) {
         if (b % 2 == 1)
@@ -44,20 +42,18 @@ int64_t montgomery_multiplication(int64_t a, int64_t b, int64_t modulus) {
  *   - modulus: The modulus.
  * Returns: The modular inverse of the number, or 0 if it does not exist.
  */
-int64_t compute_modular_inverse(int64_t number, int64_t modulus) {
-    int64_t t = 0, new_t = 1;
-    int64_t r = modulus, new_r = number;
-
-    printf("in compute modular inverse\n");
+uint32_t compute_modular_inverse(uint32_t number, uint32_t modulus) {
+    uint32_t t = 0, new_t = 1;
+    uint32_t r = modulus, new_r = number;
 
     while (new_r != 0) {
-        int64_t quotient = r / new_r;
+        uint32_t quotient = r / new_r;
 
-        int64_t temp_t = new_t;
+        uint32_t temp_t = new_t;
         new_t = t - quotient * new_t;
         t = temp_t;
 
-        int64_t temp_r = new_r;
+        uint32_t temp_r = new_r;
         new_r = r - quotient * new_r;
         r = temp_r;
     }
@@ -83,14 +79,13 @@ int64_t compute_modular_inverse(int64_t number, int64_t modulus) {
  *   - m: The number of bits.
  * Returns: The reduced result after Montgomery modular reduction.
  */
-int64_t montgomery_modular_reduction(int64_t result, int64_t modulus, int64_t Y, int64_t m) {
-    int64_t factor = (1ULL << m) % modulus;
-    printf("in montgomery modular reduction\n");
-    int64_t R_inverse = compute_modular_inverse(factor, modulus); // Compute the modular inverse of the Montgomery factor R
+uint32_t montgomery_modular_reduction(uint32_t result, uint32_t modulus, uint32_t Y, uint32_t m) {
+    uint32_t factor = (1ULL << m) % modulus;
+    uint32_t R_inverse = compute_modular_inverse(factor, modulus); // Compute the modular inverse of the Montgomery factor R
 
-    int64_t montgomery_result = montgomery_multiplication(result, 1, modulus); // Convert the result to Montgomery form
+    uint32_t montgomery_result = montgomery_multiplication(result, 1, modulus); // Convert the result to Montgomery form
 
-    int64_t reduced_result = montgomery_multiplication(montgomery_result, R_inverse, modulus); // Perform the Montgomery reduction
+    uint32_t reduced_result = montgomery_multiplication(montgomery_result, R_inverse, modulus); // Perform the Montgomery reduction
 
     return reduced_result;
 }
@@ -105,11 +100,10 @@ int64_t montgomery_modular_reduction(int64_t result, int64_t modulus, int64_t Y,
  *   - m: The number of bits.
  * Returns: The result of the modular exponentiation.
  */
-int64_t montgomery_modular_exponentiation(int64_t base, int64_t exponent, int64_t modulus, int64_t Y, int64_t m) {
-    int64_t result = 1;
-    int64_t R = (1ULL << m) % modulus;
-    printf("in modular exponentiation\n");
-    int64_t baseMont = montgomery_multiplication(base, R, modulus);  // R is the Montgomery factor
+uint32_t montgomery_modular_exponentiation(uint32_t base, uint32_t exponent, uint32_t modulus, uint32_t Y, uint32_t m) {
+    uint32_t result = 1;
+    uint32_t R = (1ULL << m) % modulus;
+    uint32_t baseMont = montgomery_multiplication(base, R, modulus);  // R is the Montgomery factor
 
     while (exponent > 0) {
         if (exponent & 1) {
@@ -125,14 +119,14 @@ int64_t montgomery_modular_exponentiation(int64_t base, int64_t exponent, int64_
 
 int main() {
     /** initial test **/
-    int64_t P = 61; // Prime Number P
-    int64_t Q = 53; // Prime Number Q
-    int64_t N = P * Q; // Modulus N
-    int64_t phi = (P - 1) * (Q - 1); // Euler's totient function value
-    int64_t E = 17; // Public exponent
-    int64_t plaintext = 440;
-    int64_t m = 64; // 64 bits
-    int64_t D;
+    uint32_t P = 61; // Prime Number P
+    uint32_t Q = 53; // Prime Number Q
+    uint32_t N = P * Q; // Modulus N
+    uint32_t phi = (P - 1) * (Q - 1); // Euler's totient function value
+    uint32_t E = 17; // Public exponent
+    uint32_t plaintext = 440;
+    uint32_t m = 64; // 64 bits
+    uint32_t D;
 
     printf("in main\n");
     printf("value of P: %11d\n", P);
@@ -150,7 +144,7 @@ int main() {
     }
 
     // Calculate the private exponent d using the formula D = (X(P-1)(Q-1) + 1) / E
-    int64_t X = 1;
+    uint32_t X = 1;
     while ((X * (P - 1) * (Q - 1) + 1) % E != 0) {
         X++;
     }
@@ -161,37 +155,37 @@ int main() {
     // Private Key: (D, PQ)
 
     // Compute R = (2^m) % N
-    int64_t R = 1;
+    uint32_t R = 1;
     int i;
     for (i = 0; i < m; i++) {
         R = (R << 1) % N;
     }
 
     // Compute Y = (R^2) % N
-    int64_t Y = (R * R) % N;
+    uint32_t Y = (R * R) % N;
 
     // Perform RSA encryption
     clock_t start_encrypt = clock();
 
-    int64_t ciphertext = montgomery_modular_exponentiation(plaintext, E, N, Y, m);
+    uint32_t ciphertext = montgomery_modular_exponentiation(plaintext, E, N, Y, m);
 
     clock_t end_encrypt = clock();
     double total_time_encrypt = (double)(end_encrypt - start_encrypt) / CLOCKS_PER_SEC;
 
     printf("********** RSA Encryption **********\n");
-    printf("RSA Encryption Ciphertext: %lld\n", ciphertext);
+    printf("RSA Encryption Ciphertext: %u\n", ciphertext);
     printf("Time to execute encrypt: %.7f\n", total_time_encrypt);
 
     // Perform RSA decryption
     clock_t start_decrypt = clock();
 
-    int64_t decrypted = montgomery_modular_exponentiation(ciphertext, D, N, Y, m);
+    uint32_t decrypted = montgomery_modular_exponentiation(ciphertext, D, N, Y, m);
 
     clock_t end_decrypt = clock();
     double total_time_decrypt = (double)(end_decrypt - start_decrypt) / CLOCKS_PER_SEC;
 
     printf("********** RSA Decryption **********\n");
-    printf("Decrypted: %lld\n", decrypted);
+    printf("Decrypted: %u\n", decrypted);
     printf("Time to execute decrypt: %.7f\n", total_time_decrypt);
 
     return 0;
