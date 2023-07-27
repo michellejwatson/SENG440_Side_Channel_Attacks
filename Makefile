1c5fadfdcd6f522240c7b2e642465ab4d546c3f8
Michelle Watson <michellejwatson1@gmail.com>
2023-07-27 13:48:28 -0700

    new makefile with rsa functions

--- Makefile
+++ Makefile
@@ -1,14 +1,17 @@
 assemble_neon:
 	arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/main.c
 
-exe_neon:
+compile_neon:
 	arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -o RSA_Routine_Neon.exe /tmp/main.c
 
 neon:
-	cp ./main.c /tmp/main.c && make assemble_neon && make exe_neon
+	cp ./main.c /tmp/main.c && make assemble_neon && make compile_neon
 
 assemble_exe:
-	arm-linux-gcc -static -S /tmp/main.c 
+	arm-linux-gcc -static -S /tmp/main.c && arm-linux-gcc -static -S /tmp/RSA_functions.c
+
+compile_exe:
+	arm-linux-gcc -static -o RSA_Routine.exe /tmp/main.c /tmp/RSA_functions.c
 
 exe: 
-	cp ./main.c /tmp/main.c && make assemble_exe && arm-linux-gcc -static -o RSA_Routine.exe /tmp/main.c 
+	cp ./main.c /tmp/main.c && cp ./RSA_functions.c /tmp/RSA_functions.c && make assemble_exe && make compile_exe
---
+++ balanced_functions.c
@@ -0,0 +1,81 @@
+// Helper function to perform modular addition
+unsigned long long int montgomery_add(unsigned long long int a, unsigned long long int b, unsigned long long int modulus) {
+    unsigned long long int sum = a + b;
+    if (sum >= modulus) //need 
+        sum -= modulus;
+    return sum;
+}
+
+/**
+* Performs Montgomery modular multiplication.
+* Parameters:
+* - a: The first operand.
+* - b: The second operand.
+* - modulus: The modulus.
+* Returns: The result of the multiplication.
+*/
+unsigned long long int montgomery_multiplication(unsigned long long int a, unsigned long long int b, unsigned long long int modulus) {
+    unsigned long long int result = 0;
+    int i;
+
+    for (i = 0; i <  64; i++) {
+        if (b % 2 == 1) result = montgomery_add(result, a, modulus);
+
+        a = (a << 1) % modulus;
+        b = b >> 1;
+
+        if (a >= modulus) a = montgomery_add(a, a, modulus);
+    }
+
+    return result;
+}
+
+
+/**
+* Performs Montgomery modular reduction.
+* Parameters:
+* - result: The result of the modular exponentiation.
+* - modulus: The modulus.
+* - Y: The Montgomery factor.
+* - m: The number of bits.
+* Returns: The reduced result after Montgomery modular reduction.
+*/
+unsigned long long int montgomery_modular_reduction(unsigned long long int result, unsigned long long int modulus, unsigned long long int Y, unsigned long long int m) {
+    unsigned long long int R_inverse = 1;
+    unsigned long long int montgomery_result = montgomery_multiplication(result, 1, modulus); // Convert the result to Montgomery form
+
+    unsigned long long int reduced_result = montgomery_multiplication(montgomery_result, R_inverse, modulus); // Perform the Montgomery reduction
+
+    return reduced_result;
+}
+
+/**
+* Performs Montgomery modular exponentiation.
+* Parameters:
+* - base: The base value.
+* - exponent: The exponent value.
+* - modulus: The modulus.
+* - Y: The Montgomery factor.
+* - m: The number of bits.
+* Returns: The result of the modular exponentiation.
+*/
+unsigned long long int montgomery_modular_exponentiation(unsigned long long int base, unsigned long long int exponent, unsigned long long int modulus, unsigned long long int Y, unsigned long long int m) {
+    unsigned long long int result = 1;
+    unsigned long long int R = 1;
+    unsigned long long int baseMont = montgomery_multiplication(base, R, modulus); // R is the Montgomery factor
+    unsigned long long int dummy = 0;
+
+    while (exponent > 0) {
+        if (exponent & 1) {
+            result = montgomery_multiplication(result, baseMont, modulus);
+        }
+        else {
+            dummy = montgomery_multiplication(result, baseMont, modulus);
+        }
+
+        baseMont = montgomery_multiplication(baseMont, baseMont, modulus);
+        exponent >>= 1;
+    }
+
+    return montgomery_modular_reduction(result, modulus, Y, m); // Montgomery reduction after exponentiation
+}
