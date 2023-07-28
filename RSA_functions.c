5cea6baba85863241fb2f1afb8b7617ad1dc5c21
haleyrutten <86267365+haleyrutten@users.noreply.github.com>
2023-07-28 21:42:23 +0000

    balanced if in addition function

--- RSA_functions.c
+++ RSA_functions.c
@@ -11,8 +11,11 @@
 // to help with overflow
 unsigned long long int montgomery_add(unsigned long long int a, unsigned long long int b, unsigned long long int modulus) {
     unsigned long long int sum = a + b;
+    unsigned long long int dummy;
     if (sum >= modulus){
         sum -= modulus;
+    } else {
+        dummy -= modulus;
     }
     return sum;
 }
